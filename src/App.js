7eb4cbbba2b28616d36cba14a186ecc0d8f37aa5
KazbekovAktenir <kazbekovaktenir@gmail.com>
2024-06-28 15:03:19 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Navbar from "./components/homePage/Navbar";
+import { Router } from "react-router-dom";
 
 const App = () => {
-  return <div></div>;
+  return (
+    // <Router>
+
+    // </Router>
+    <Navbar />
+  );
 };
 
 export default App;
